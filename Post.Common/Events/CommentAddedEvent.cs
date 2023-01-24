8d8e1ca131cf813e949734cb6248b0b3d93e7c05
phamdangtrung <55026963+phamdangtrung@users.noreply.github.com>
2023-01-24 13:36:32 +0700

    Add events

---
+++ Post.Common/Events/CommentAddedEvent.cs
@@ -0,0 +1,14 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+public class CommentAddedEvent : BaseEvent
+{
+	public CommentAddedEvent() : base(nameof(CommentAddedEvent))
+	{
+	}
+
+	public Guid CommentId { get; set; } = Guid.Empty;
+	public string Comment { get; set; } = string.Empty;
+	public string Username { get; set; } = string.Empty;
+	public DateTime CommentDate { get; set; }
+}
---
+++ Post.Common/Events/CommentUpdatedEvent.cs
@@ -0,0 +1,14 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+public class CommentUpdatedEvent : BaseEvent
+{
+	public CommentUpdatedEvent() : base(nameof(CommentUpdatedEvent))
+	{
+	}
+
+	public Guid CommentId { get; set; } = Guid.Empty;
+	public string Comment { get; set; } = string.Empty;
+	public string Username { get; set; } = string.Empty;
+	public DateTime EditDate { get; set; }
+}
