fd6a233a5533699a4443c87d800b7ff0c5110f0a
phamdangtrung <55026963+phamdangtrung@users.noreply.github.com>
2023-01-27 18:52:14 +0700

    Add IEventStoreRepository and EventStoreRepository

---
+++ CQRS.Core/Events/EventModel.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace CQRS.Core.Events;
+public class EventModel
+{
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+    public DateTime Timestamp { get; set; }
+    public Guid AggregateIdentifier { get; set; } = Guid.Empty;
+    public string AggregateType { get; set; } = string.Empty;
+    public int Version { get; set; }
+    public string EventType { get; set; } = string.Empty;
+    public BaseEvent EventData { get; set; }
+}
